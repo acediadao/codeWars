5ce1ad2a2345dba60f786effdf1acebd00d037da
Chase Acedia <acediadao@gmail.com>
2026-01-30 17:14:41 -0700

    invert values

---
+++ invert.js
@@ -0,0 +1,5 @@
+/*
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+*/
+
+const invertValue = array => array.map((x) => x * -1)
