d446b0773df2577de893936825eb2b9d47bf21b4
Chase Acedia <acediadao@gmail.com>
2026-02-01 11:32:37 -0700

    return summation of numbers from 1

---
+++ summation.js
@@ -0,0 +1,11 @@
+/*
+Write a program that finds the summation of every number from 1 to num (both inclusive). The number will always be a positive integer greater than 0.
+*/
+
+function summation(num) {
+    let sum = 0;
+    for(i=1;i<=num;i++) {
+        sum += i;
+    }
+    return sum;
+}
