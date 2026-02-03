66c35da4fdf17d5aa8c2e1d07031aa90c1a16772
Chase Acedia <acediadao@gmail.com>
2026-02-03 14:52:13 -0700

    convert a string to a number

---
+++ stringToNumber.js
@@ -0,0 +1,7 @@
+/*
+We need a function that can transform a string into a number. What ways of achieving this do you know?
+*/
+
+const strToNum = str => Number(str)
+
+console.log(strToNum("4422"))
